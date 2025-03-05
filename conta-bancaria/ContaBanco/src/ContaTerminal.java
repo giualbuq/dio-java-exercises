34e5709fb3aba00aa59541885f501a7839a95514
giualbuq <giulia.albsilva@gmail.com>
2025-03-05 19:52:54 -0300

    Exercicio da conta bancaria

---
+++ conta-bancaria/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o número da Numero da conta: ");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número do Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é" + agencia+ ", conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque");
+        scanner.close();
+    }
+}
